0539bcf183f18dfaf83e8692d6449e5465a631e0
Phil Marcus <pmarcus@reverek12.org>
2024-02-01 09:10:14 -0500

    working on threat

--- BotBattler/src/game/Threat.java
+++ BotBattler/src/game/Threat.java
@@ -4,6 +4,50 @@ public class Threat {
 
 	//array containing incoming damage for each quadrant
 	private int[] vals;
+	
+	//The number of sides on the die to roll to determine the base threat in each quadrant
+	private final int baseThreat = 10;
+	
+	//Every x turns, one quadrant will have extra threat!
+	private final int spikeFrequency = 4; 
+	
+	//When there's "spike" threat, this is how many extra dice to roll
+	private final int spikeDice = 5; 
+	
+	public Threat() {
+		
+		//There are always four quadrants, so a size four array will store the threat.
+		vals = new int[4];
+		
+		//roll a die for the threat in each quadrant
+		for(int i=0;i<4;i++) {
+			vals[i] = rollDie(baseThreat);
+		}
+		
+		//Sometimes, a quadrant will have extra "spike" threat.
+		if (rollDie(spikeFrequency) == spikeFrequency) {
+			//choose a random quadrant
+			int q = rollDie(4);
+			//add spike threat to that quadrant
+			va
+		}
+		
+	}
+	
+	//rolls an x-sided die
+	//returns a random int between 1 and sides
+	private int rollDie(int sides) {
+		return (int)(Math.random()*sides +1);
+	}
+	
+	//rolls any number of x-sided dice
+	private int rollDice(int sides, int num ) {
+		int sum = 0;
+		for(int i=0;i<num;i++) {
+			sum += rollDie(sides);
+		}
+		return sum;
+	}
 
 	//returns the threatened damage in quadrant q
 	public int getQuadrantThreat(int q) {
