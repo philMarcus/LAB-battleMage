18825ea472f20894a3a12916c6ce7dc214b5a883
Phil Marcus <pmarcus@reverek12.org>
2024-02-01 07:40:37 -0500

    comments

--- BotBattler/src/characters/Character.java
+++ BotBattler/src/characters/Character.java
@@ -8,10 +8,19 @@ import game.Threat;
 //The game of battleMage is played by writing a Java class to
 //implement this character interface and then constructing an
 //instance of your character in the arena to see how many increasingly
-//tough battles it can survive.
+//tough opponents it can survive.
 //
 //WHO WILL WRITE THE STRONGEST BATTLEMAGE?!?!?
 //
+//To implement this interface, make sure of two things:
+//
+//1) your class header has "implements Character"
+//e.g. "public class YourClass implements Character{..."
+//and
+//2) Your class must contain the methods listed below, using the signatures exactly
+//as written here.
+//
+//Everything else is up to you!
 public interface Character {
 
 	public Resource getHitPointResource();
