7693fe62718f0fff1bf03b091ffa45f581002ff7
Phil Marcus <pmarcus@reverek12.org>
2024-02-15 09:32:14 -0500

    Added Arena Challenge

---
+++ BotBattler/src/game/ArenaChallenge.java
@@ -0,0 +1,43 @@
+package game;
+
+import characters.BadBlocker;
+import characters.DumbFighter;
+import characters.GoodBlocker;
+import characters.Harrison;
+import characters.Joseph;
+import characters.SadMage;
+import characters.BattleMageChampion;
+import characters.Blaster;
+
+public class ArenaChallenge {
+
+	private static int num = 100000;
+
+	// runs battles with increasing opponent level until player dies
+	// returns the level on which the player died
+	public static int runChallenge() {
+		int level = 1;
+		boolean isOver = false;
+		while (!isOver) {
+			Harrison player = new Harrison();
+			Battle b = new Battle(player, level);
+			if (b.doBattle(false, false))
+				level++;
+			else
+				isOver = true;
+		}
+		return level;
+	}
+
+	public static void main(String[] args) {
+		int totalLevels = 0;
+		
+		for (int i = 0; i < num; i++) {
+			int lev = runChallenge();
+			totalLevels += lev;
+			System.out.println("Died on level "+lev+"\n");
+
+		}
+		System.out.println("Average Level: "+(double)totalLevels/num);
+	}
+}
