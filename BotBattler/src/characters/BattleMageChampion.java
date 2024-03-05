1ee39a6e2f2adcad7cb0baee2b2323f228dd5a5e
Phil Marcus <pmarcus@reverek12.org>
2024-03-05 18:40:57 -0500

    195/5 performs better at the Arena Challenge than 197/3 HP/Stam (24.8 vs 24.3)

--- BotBattler/src/characters/BattleMageChampion.java
+++ BotBattler/src/characters/BattleMageChampion.java
@@ -15,8 +15,8 @@ import game.Threat;
 public class BattleMageChampion implements Character {
 
 	// We plan to use HP for all actions except blocks.
-	private Resource hp = new Resource("HP", 197);
-	private Resource stamina = new Resource("Stamina", 3);
+	private Resource hp = new Resource("HP", 195);
+	private Resource stamina = new Resource("Stamina", 5);
 
 	int blockableDmg; // the largest amount of dmg that could be blocked
 
